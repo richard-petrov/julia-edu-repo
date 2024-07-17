28313d30e1b1c21e9eee33e35271f3320883c7e1
Richard Petrov <rihard.petroff@gmail.com>
2024-07-17 14:47:16 +0400

    initial-commit

---
+++ pluto.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.17.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f4231d4e-e58f-4e1c-83b6-57d1e9502388
+
+
+# ╔═╡ 80c7ef40-4425-11ef-1114-25df0457368c
+println("Hello, Julia")
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═f4231d4e-e58f-4e1c-83b6-57d1e9502388
+# ╠═80c7ef40-4425-11ef-1114-25df0457368c
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
